0843216011884e9f8acd3110fc22ce10391dd961
Nikhil Rathore <104098077+Fake098@users.noreply.github.com>
2024-08-10 09:48:56 +0530

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "PrismCore",
+  description:
+    "PrismCore is a game development organization that provides a robust game engine in C++.",
+  url: "https://prismcore.vercel.app",
+  ogImage: "https://prismcore.vercel.app/og.jpg",
+  links: {
+    twitter: "https://twitter.com/avalynndev",
+    github: "https://github.com/PrismCoreGD/PrismCore",
+  },
+}
